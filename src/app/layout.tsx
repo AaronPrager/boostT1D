b467084a16aa2ab6ecbb1680ec2fada49c88621d
Aaron Prager <arik@pragersfamily.com>
2025-10-16 17:07:26 -0400

    Update web icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,28 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'BoostT1D ⚡ - Smart Diabetes Management',
   description: 'Empowering Type 1 Diabetes Management with Smart Analytics, Community Connection, and Real-time Monitoring',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/favicon-64x64.png', sizes: '64x64', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon-128x128.png', sizes: '128x128', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon-57x57.png', sizes: '57x57', type: 'image/png' },
+      { url: '/apple-touch-icon-60x60.png', sizes: '60x60', type: 'image/png' },
+      { url: '/apple-touch-icon-72x72.png', sizes: '72x72', type: 'image/png' },
+      { url: '/apple-touch-icon-76x76.png', sizes: '76x76', type: 'image/png' },
+      { url: '/apple-touch-icon-114x114.png', sizes: '114x114', type: 'image/png' },
+      { url: '/apple-touch-icon-120x120.png', sizes: '120x120', type: 'image/png' },
+      { url: '/apple-touch-icon-144x144.png', sizes: '144x144', type: 'image/png' },
+      { url: '/apple-touch-icon-152x152.png', sizes: '152x152', type: 'image/png' },
+      { url: '/apple-touch-icon-180x180.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/manifest.json',
 };
 
 export default async function RootLayout({
